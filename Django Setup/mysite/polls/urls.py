1b3bfc794636414d7e59f50515fa333f6aa7943b
John Hunter <JHunte@uw.edu>
2022-09-18 08:03:15 -0700

    adding the files to the local repo and the pushing to git

---
+++ Django Setup/mysite/polls/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 16:50:43 2022
+
+@author: johnh
+"""
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
